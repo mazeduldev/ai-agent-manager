06bfc42176f4b4506e21bf50fe221ea8413af623
Mazedul Islam <mazidmailbox@gmail.com>
2025-09-15 11:43:21 +0600

    fe(dashboard): removed unused code

--- frontends/dashboard/src/app/layout.tsx
+++ frontends/dashboard/src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "sonner";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import AppProvider from "@/components/providers/AppProvider";
 
 const geistSans = Geist({
@@ -19,8 +18,6 @@ export const metadata: Metadata = {
 	title: "AI Agent Dashboard",
 	description: "A Place to Manage Your AI Agents",
 };
-
-const queryClient = new QueryClient();
 
 export default function RootLayout({
 	children,
